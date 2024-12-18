cd78fe91c13425a356f137e06b3b366c5b7ae947
Connor Ward <c.ward20@imperial.ac.uk>
2024-12-18 14:49:11 +0000

    From feedback

--- pytest_mpi/parallel_assert.py
+++ pytest_mpi/parallel_assert.py
@@ -1,18 +1,28 @@
+from collections.abc import Callable
+
 from mpi4py import MPI
 
-def parallel_assert(assertion, subset=None, msg=""):
-    """Make an assertion across MPI.COMM_WORLD
 
-    Parameters:
-    -----------
-    assertion:
+def parallel_assert(assertion: Callable, participating: bool = True, msg: str = "") -> None:
+    """Make an assertion across ``COMM_WORLD``.
+
+    Parameters
+    ----------
+    assertion :
         Callable that will be tested for truthyness (usually evaluates some assertion).
         This should be the same across all ranks.
-    subset:
+    participating :
+        Whether the given rank should evaluate the assertion.
+    msg :
+        Optional error message to print out on failure.
 
+    Notes
+    -----
+    It is very important that ``parallel_assert`` is called collectively on all
+    ranks simulataneously.
 
-    Example:
-    --------
+    Example
+    -------
     Where in serial code one would have previously written:
     ```python
     x = f()
@@ -24,17 +34,13 @@ def parallel_assert(assertion, subset=None, msg=""):
     x = f()
     parallel_assert(lambda: x < 5)
     ```
+
     """
-    if subset:
-        if MPI.COMM_WORLD.rank in subset:
-            evaluation = assertion()
-        else:
-            evaluation = True
-    else:
-        evaluation = assertion()
-    all_results = MPI.COMM_WORLD.allgather(evaluation)
+    result = assertion() if participating else True
+    all_results = MPI.COMM_WORLD.allgather(result)
     if not min(all_results):
         raise AssertionError(
             "Parallel assertion failed on ranks: "
-            f"{[ii for ii, b in enumerate(all_results) if not b]}\n" + msg
+            f"{[rank for rank, result in enumerate(all_results) if not result]}\n"
+            + msg
         )
